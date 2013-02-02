50379bd3b71d85f3cc11e5ecaaa6d18bb4aff1d8
Alexey Velikiy <ilms@live.ru>
2013-02-02 05:19:58 +0400

    initTast self-path resolve

--- CFCPP/buildServer/initTask.sh
+++ CFCPP/buildServer/initTask.sh
@@ -1,4 +1,5 @@
-work_port="$(cat .sessionCfg)"
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+work_port="$(cat $DIR/.sessionCfg)"
 wget "127.0.0.1:$work_port" -q -b
 rm wget-log*
 rm index*
