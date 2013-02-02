523c5db38e3d5bd344a41485f996abd6d38f6c33
Alexey Velikiy <ilms@live.ru>
2013-02-02 06:57:14 +0400

    DOWNTO example

--- CFCPP/solution.cpp
+++ CFCPP/solution.cpp
@@ -5,6 +5,7 @@
 #include <algorithm>
 
 #define REP(i, to) for(int i = 0; i < to; i++)
+#define DOWNTO(i, to) for(int i = to; i >= 0; i--)
 #define FOR(it,c) for ( __typeof((c).begin()) it=(c).begin(); it!=(c).end(); it++ )
 #define vi vector<int>
 
@@ -14,14 +15,21 @@ int main() {
 	
 	int n;
 	cin >> n;
+	int a[100010];
 	
 	REP(i, n) {
-		int angle = 0;
-		cin >> angle;
-		if((360 % (180 - angle)) == 0){
-			cout << "YES\n";
-		} else cout << "NO\n";
+		int ss = 0;
+		scanf("%d", &ss);
+		a[i] = ss;
 	}
+	
+	int m = 1;
+	DOWNTO(i, n-2) {
+		if(a[i] < a[i+1]) { m++; }
+		else break;
+	}
+	
+	cout << (n - m) << endl;
 		
 }
 
