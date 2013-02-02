51f086247f832c3d171d70ebdb540f1f79f446ca
Alexey Velikiy <ilms@live.ru>
2013-02-02 06:19:56 +0400

    index.htm stub

--- CFCPP/buildServer/initTask.sh
+++ CFCPP/buildServer/initTask.sh
@@ -1,5 +1,4 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 work_port="$(cat $DIR/.sessionCfg)"
-wget "127.0.0.1:$work_port" -q -b
-rm wget-log*
-rm index*
+wget "127.0.0.1:$work_port" -q -b & rm wget-log* & rm index.html*
+
