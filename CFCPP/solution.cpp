3294bea4d33d307c01c787b87d60998334773d55
Alexey Velikiy <ilms@live.ru>
2013-03-27 12:52:15 +0400

    to string function

--- CFCPP/solution.cpp
+++ CFCPP/solution.cpp
@@ -3,54 +3,44 @@
 #include <vector>
 #include <stdio.h>
 #include <algorithm>
+#include <ctype.h>
+#include <limits.h>
+#include <stdlib.h>
+#include <map>
+#include <set>
+#include <math.h>
+#include <sstream>
 
 #define REP(i, to) for(__typeof(to) i = 0; i < to; i++)
 #define DOWNTO(i, to) for(int i = to; i >= 0; i--)
 #define FOR(it,c) for ( __typeof((c).begin()) it=(c).begin(); it!=(c).end(); it++ )
-#define vi vector<int>
-
+template<class T> std::string toStr(T t);
+ 
 using namespace std;
 
+int stat[10];
+
 int main() {
-	
-	int n;
-	cin >> n;
-	int a[100010];
-	
-	REP(i, n) {
-		int ss = 0;
-		scanf("%d", &ss);
-		a[i] = ss;
-	}
-	
-	int m = 1;
-	DOWNTO(i, n-2) {
-		if(a[i] < a[i+1]) { m++; }
-		else break;
-	}
-	
-	cout << (n - m) << endl;
-		
+    
+    string s;
+    cin >> s;
+    long long vb;
+    cin >> vb;
+    
+    cout << toStr(vb+1);
+    
+
 }
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+template<class T>
+string toStr(T t){
+    std::stringstream strstream;
+    string resStr;
+    strstream << t;
+    strstream >> resStr;
+    return resStr;
+}
 
 
 
